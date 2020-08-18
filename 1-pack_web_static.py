8d69ef282fe502db78d5afae9ac280d0be8c7f01
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-18 04:22:29 +0000

    Task 1 deploy_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Generate .tgz archive from the contents of the web_static"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Compress files"""
+    try:
+        local("mkdir -p versions")
+        now = datetime.now()
+        current_date = now.strftime("%Y%m%d%H%M%S")
+        f_path = "versions/web_static_" + current_date
+        local("tar -cvzf {}.tgz web_static"
+              .format(f_path))
+        return f_path
+    except:
+        return None
